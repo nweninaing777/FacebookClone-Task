cdcdaa1b61a9f2ebf2ae82c95a596225f9fdcd45
nwe ni naing <nweninaing896@gmail.com>
2020-10-04 02:58:39 -0400

    new function create

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   root 'users#new'
-
-    resources :sessions, only: [:new, :create, :destroy]
-    resources :users
-  
-    resources :pictures do
-        collection do
-          post :confirm
-        end
-      end
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  resources :pictures do
+    collection do
+      post :confirm
+    end
   end
+end
