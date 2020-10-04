f06491340d55d826188dea889739b25fd45b54d0
nwe ni naing <nweninaing896@gmail.com>
2020-10-04 12:04:52 -0400

    facebookclone edit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+//require("@rails/ujs").start()
+//require("turbolinks").start()
+//require("@rails/activestorage").start()
+//require("channels")
