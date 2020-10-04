de0e52a5d5613056440911d198fdcb1fbdecbc1e
nwe ni naing <nweninaing896@gmail.com>
2020-10-04 02:54:36 -0400

    facebookclone edit

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,59 +1,56 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @pictures = Picture.all
+  end
 
- def index
-   @pictures = Picture.all
- end
+  def new
+    @picture = Picture.new
+  end
 
- def new
-   @picture = Picture.new
- end
+  def show
+  end
 
- def show
- end
+  def edit
+  end
 
- def edit
- end
-
- def create
-     @picture = current_user.pictures.build(picture_params)
-     if params[:back]
-       render :new
-     else
-       if @picture.save
-         redirect_to pictures_path, notice: "Post was successfully created."
-       else
-         render :new
-       end
-     end
-   end
-
-
-   def update
-      if @picture.update(picture_params)
-        redirect_to pictures_path, notice: "Edited post！"
+  def create
+      @picture = current_user.pictures.build(picture_params)
+      if params[:back]
+        render :new
       else
-        render :edit
+        if @picture.save
+          redirect_to pictures_path, notice: "Post was successfully created."
+        else
+          render :new
+        end
       end
     end
 
-    def destroy
-    @picture.destroy
-    redirect_to pictures_path, notice:"Deleted post!"
-  end
+    def update
+       if @picture.update(picture_params)
+         redirect_to pictures_path, notice:"Edited post！"
+       else
+         render :edit
+       end
+     end
 
-  def confirm
-    @picture = Picture.new(picture_params)
+     def destroy
+     @picture.destroy
+     redirect_to pictures_path, notice:"Deleted post!"
    end
 
+   def confirm
+     @picture = Picture.new(picture_params)
+    end
 
- private
- def set_picture
-   @picture = Picture.find(params[:id])
- end
+  private
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
 
- def picture_params
-   params.require(:picture).permit(:image, :content,:image_cache)
- end
+  def picture_params
+    params.require(:picture).permit(:image, :content,:image_cache)
+  end
 end
